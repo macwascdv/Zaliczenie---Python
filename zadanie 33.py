c9f42ce6e856c08c06ecc4a696d5d0242e5009ea
Maciej Wąsiatycz <56920706+macwascdv@users.noreply.github.com>
2020-01-28 21:04:36 +0100

    Update zadanie 33.py

--- zadanie 33.py
+++ zadanie 33.py
@@ -2,41 +2,13 @@
 #Sprawdzenie ilosci zywych komore - m to macierz
 import numpy as np
 def mk(n):
-    return np.random.choice([255,0], size=n**2, p=[0.4,0.6]).reshape(n,n)
-def addGlider(i, j, grid):
-    glider = np.array([[0, 0, 255], [255, 0, 255], [0, 255, 255]])
-    grid[i:i+3, j:j+3] = glider
-    return grid
+    return np.random.choice([0], size=n**2,).reshape(n,n)
+
 def addBochenek(i,j,grid):
     loaf = np.array([[0,255,255,0],[255,0,0,255],[0,255,0,255],
                       [0,0,255,0]])
     grid[i:i+4, j:j+4] = loaf
     return grid
-def addCegla(i,j,grid):
-    block = np.array([[255,255],[255,255]])
-    grid[i:i+2, j:j+2] = block
-    return grid
-def addBeehive(i,j,grid):
-    beehive = np.array([[0,255,255,0],[255,0,0,255],[0,255,255,0]])
-    grid [i:i+3,j:j+4] = beehive
-    return grid
-def addBoat(i,j,grid):
-    boat = np.array([[255,255,0],[255,0,255],[0,255,0]])
-    grid [i:i+3,j:j+3] = boat
-    return grid
-def addBlinker(i,j,grid):
-    blinker = np.array([[255],[255],[255]])
-    grid [i:i+3,j:j] = blinker
-    return grid
-def addToad(i,j,grid):
-    toad = np.array([[0,255,255,255],[255,255,255,0]])
-    grid [i:i+2,j:j+4] = toad
-    return grid
-def addBeacon(i,j,grid):
-    beacon = np.array([[255,255,0,0],[255,0,0,0],[0,0,0,255],[0,0,255,255]])
-    grid [i:i+4,j:j+4] = beacon
-    return grid
-
 
 def doa(i,j,macierz):
     def komorka(i, j):
@@ -62,3 +34,9 @@ def doa(i,j,macierz):
         sasiedzi += 1
 
     return sasiedzi
+
+macierz=mk(6)
+
+macierz=addBochenek(2,2,macierz)
+print (macierz)
+print(doa(3,3,macierz))
